71686fd7c24dfbd097bfb4f91ae7c7a6505f58fb
timbastin <bastin.tim@gmail.com>
2022-09-06 10:57:30 +0200

    Added jest, added HttpInspector

--- src/inspect/DefaultUrlInspector.ts
+++ src/inspect/DefaultUrlInspector.ts
@@ -1,16 +1,25 @@
 import { InspectResult, ResponseInspector, UrlInspector } from "./Inspector";
 
-export default class DefaultUrlInspector implements UrlInspector {
-  constructor(private readonly responseInspectors: ResponseInspector[]) {}
-  async inspect(url: URL): Promise<InspectResult[]> {
-    const response = await fetch(url.toString(), {
+export default class DefaultUrlInspector extends UrlInspector {
+  constructor(
+    private readonly responseInspectors: ResponseInspector[],
+    httpClient: typeof fetch
+  ) {
+    super(httpClient);
+  }
+  async inspect(hostname: string): Promise<InspectResult[]> {
+    // use https as protocol.
+    const url = new URL(`https://${hostname}`);
+    const response = await this.httpClient(url.toString(), {
       method: "GET",
     });
 
-    return await Promise.all(
-      this.responseInspectors.map((responseInspector) =>
-        responseInspector.inspect(response)
+    return (
+      await Promise.all(
+        this.responseInspectors.map((responseInspector) =>
+          responseInspector.inspect(response)
+        )
       )
-    );
+    ).flat();
   }
 }
--- src/inspect/Inspector.ts
+++ src/inspect/Inspector.ts
@@ -1,9 +1,57 @@
+export enum InspectionType {
+  HTTP,
+  HTTP_308,
+  HTTP_LocationHttps,
+  HTTPS,
+}
+
+export interface InspectResultDTO {
+  type: InspectionType;
+  didPass: boolean;
+  subResults?: InspectResultDTO[];
+}
+
+export class InspectResult {
+  type: InspectionType;
+  didPass: boolean;
+  subResults?: Array<InspectResult>;
+
+  constructor(
+    type: InspectionType,
+    didPass: undefined,
+    subResults: Array<InspectResult>
+  );
+  constructor(type: InspectionType, didPass: boolean);
+  constructor(
+    type: InspectionType,
+    didPass?: boolean,
+    subResults?: Array<InspectResult>
+  ) {
+    this.type = type;
+    this.didPass = Boolean(
+      didPass || subResults?.every((subResult) => subResult.didPass)
+    );
+    this.subResults = subResults;
+  }
+
+  toDTO(): InspectResultDTO {
+    return {
+      type: this.type,
+      didPass: this.didPass,
+      subResults: this.subResults?.map((subResult) => subResult.toDTO()),
+    };
+  }
+}
+
 export interface InspectResult {}
 
 export interface ResponseInspector {
-  inspect(response: Response): Array<InspectResult | Promise<InspectResult>>;
+  inspect(response: Response): Promise<InspectResult>;
 }
 
-export interface UrlInspector {
-  inspect(url: URL): Array<InspectResult> | Promise<Array<InspectResult>>;
+export abstract class UrlInspector {
+  // simplifies testing since we can inject our own http client.
+  constructor(protected readonly httpClient: typeof fetch) {}
+
+  abstract inspect(hostname: string): Promise<InspectResult[]>;
 }
