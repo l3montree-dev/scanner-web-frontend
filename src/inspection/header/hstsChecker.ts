2a40f2487b2423a6b1a8c3d6011db4fd11b4873a
sebastian <kawelkesebastian@gmail.com>
2022-10-13 14:58:54 +0200

    Added requirements for caa, dnssec and hsts

--- src/inspection/header/hstsChecker.ts
+++ src/inspection/header/hstsChecker.ts
@@ -1,5 +1,15 @@
 import { HeaderInspectionType, InspectionResult } from "../Inspector";
 
+/**
+ *
+ * @requirements
+ * REQUIRED: the "Strict-Transport-Security" response header is present.
+ * REQUIRED: the "max-age=<expire-time>" directive is present.
+ * RECOMMENDED: the "includeSubDomains" directive SHOULD be present.
+ *
+ * Example: Strict-Transport-Security: max-age=31536000; includeSubDomains
+ *
+ */
 export const hstsChecker = (response: Response) => {
   return new InspectionResult(
     HeaderInspectionType.HSTS,
