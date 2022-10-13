2ab9888010a184ac4d0931338aa42af1f7a79a50
timbastin <bastin.tim@gmail.com>
2022-10-13 12:17:19 +0200

    added meta tags

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -81,6 +81,31 @@ const Home: NextPage = () => {
     <Page>
       <Head>
         <title>OZG Security Challenge 2023</title>
+        <meta
+          name="description"
+          content="OZG-Security Schnelltest einer Webseite in Bezug auf IT-Sicherheitsmaßnahmen und Best-Practices"
+        />
+        <meta name="title" content="OZG-Security Schnelltest einer Webseite" />
+        <meta
+          name="keywords"
+          content="OZG, Security, IT-Security, Schnelltest, IT-Sicherheit, Onlinezugangsgesetz, Security-Challenge, OZG-Security-Challenge 2023, Best-Practices, Website Scan"
+        />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://ozgsec.de/" />
+        <meta property="og:title" content="OZG Security Challenge 2023" />
+        <meta
+          property="og:description"
+          content="OZG-Security Schnelltest einer Webseite in Bezug auf IT-Sicherheitsmaßnahmen und Best-Practices"
+        />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://ozgsec.de/" />
+        <meta property="twitter:title" content="OZG Security Challenge 2023" />
+        <meta
+          property="twitter:description"
+          content="OZG-Security Schnelltest einer Webseite in Bezug auf IT-Sicherheitsmaßnahmen und Best-Practices"
+        />
       </Head>
       <div className="flex pb-10 flex-col w-full justify-center">
         <div className="max-w-screen-lg md:p-5 mx-auto">
