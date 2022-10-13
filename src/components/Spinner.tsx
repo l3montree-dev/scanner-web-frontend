dfb47516940cc076a861a0188e50603a31bd7fe4
timbastin <bastin.tim@gmail.com>
2022-10-13 12:21:16 +0200

    Inline svg

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,7 +1,34 @@
 import React from "react";
 
 const Spinner = () => {
-  return <img src="/assets/spinner.svg" alt="Loading Spinner" />;
+  return (
+    <svg
+      width="37"
+      height="37"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 100 100"
+      preserveAspectRatio="xMidYMid"
+    >
+      <circle
+        cx="50"
+        cy="50"
+        fill="none"
+        stroke="#060d38"
+        strokeWidth="10"
+        r="35"
+        strokeDasharray="164.93361431346415 56.97787143782138"
+      >
+        <animateTransform
+          attributeName="transform"
+          type="rotate"
+          repeatCount="indefinite"
+          dur="1s"
+          values="0 50 50;360 50 50"
+          keyTimes="0;1"
+        ></animateTransform>
+      </circle>
+    </svg>
+  );
 };
 
 export default Spinner;
