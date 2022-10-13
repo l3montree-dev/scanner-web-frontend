f58fa7f50328379636741a33ccb15beb35efd478
sebastian <kawelkesebastian@gmail.com>
2022-10-13 15:39:28 +0200

    Added requirements for responsible disclosure

--- src/inspection/organizational/responsibleDisclosureChecker.ts
+++ src/inspection/organizational/responsibleDisclosureChecker.ts
@@ -1,5 +1,22 @@
 import { InspectionResult, OrganizationalInspectionType } from "../Inspector";
 
+/**
+ *
+ * @requirements
+ * REQUIRED: the file "/.well-known/security.txt" is present.
+ * REQUIRED: the file "/.well-known/security.txt" contains the "Contact:" field with a contact option ONCE.
+ * REQUIRED: the file "/.well-known/security.txt" contains the "Expires:" field ONCE.
+ * RECCOMENDED: the file "/.well-known/security.txt" contains the "Encryption:" field, if so only ONCE.
+ * RECCOMENDED: the file "/.well-known/security.txt" contains the "Canonical:" field, if so only ONCE.
+ * RECCOMENDED: the file "/.well-known/security.txt" contains the "Preferred-Languages:" field, if so only ONCE.
+ * RECCOMENDED: the file "/.well-known/security.txt" is signed with a valid PGP signature. https://datatracker.ietf.org/doc/html/draft-foudil-securitytxt-12#section-3.3
+ *
+ * Example: "Contact: mailto:..."
+ * Example: "Contact: tel:.."
+ * Example: "Contact: https://..."
+ * Example: "Expires: 2021-12-31T18:37:07z"
+ *
+ */
 export const responsibleDisclosureChecker = async (
   response: Response
 ): Promise<InspectionResult> => {
