03e95adb896dcf378e59131210b305dcc048760e
timbastin <bastin.tim@gmail.com>
2023-01-13 15:04:37 +0100

    Changed time functions to work with utc dates only

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -2,15 +2,15 @@ export const eachWeek = (start: Date, end: Date) => {
   // go to the start of the day
   start = new Date(start);
   end = new Date(end);
-  start.setHours(0, 0, 0, 0);
-  end.setHours(0, 0, 0, 0);
+  start.setUTCHours(0, 0, 0, 0);
+  end.setUTCHours(0, 0, 0, 0);
   const dates = [start.getTime()];
 
   let currentDate = start;
   while (currentDate < end) {
     currentDate = new Date(currentDate.getTime() + 7 * 24 * 60 * 60 * 1000);
     // go to the start of the day
-    currentDate.setHours(0, 0, 0, 0);
+    currentDate.setUTCHours(0, 0, 0, 0);
     dates.push(Math.min(currentDate.getTime(), end.getTime()));
   }
 
@@ -21,15 +21,15 @@ export const eachDay = (start: Date, end: Date) => {
   // go to the start of the day
   start = new Date(start);
   end = new Date(end);
-  start.setHours(0, 0, 0, 0);
-  end.setHours(0, 0, 0, 0);
+  start.setUTCHours(0, 0, 0, 0);
+  end.setUTCHours(0, 0, 0, 0);
   const dates = [start.getTime()];
 
   let currentDate = start;
   while (currentDate < end) {
     currentDate = new Date(currentDate.getTime() + 24 * 60 * 60 * 1000);
     // go to the start of the day
-    currentDate.setHours(0, 0, 0, 0);
+    currentDate.setUTCHours(0, 0, 0, 0);
     dates.push(Math.min(currentDate.getTime(), end.getTime()));
   }
 
