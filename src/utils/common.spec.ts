4dfc4621ea396c2bb41ff00aad103090d122ceb0
timbastin <bastin.tim@gmail.com>
2023-01-26 14:45:22 +0100

    Added test for trimming white spaces

--- src/utils/common.spec.ts
+++ src/utils/common.spec.ts
@@ -14,4 +14,18 @@ describe("common test suite", () => {
     expect(entries[0]).toEqual("domain1.com");
     expect(entries[1]).toEqual("domain2.com");
   });
+
+  it("should respect /r line breaks inside a csv file", () => {
+    const data = `domain1.com\rdomain2.com`;
+    const entries = splitLineBreak(data);
+    expect(entries[0]).toEqual("domain1.com");
+    expect(entries[1]).toEqual("domain2.com");
+  });
+
+  it("should remove leading and trailing white spaces", () => {
+    const data = `domain1.com\ndomain2.com `;
+    const entries = splitLineBreak(data);
+    expect(entries[0]).toEqual("domain1.com");
+    expect(entries[1]).toEqual("domain2.com");
+  });
 });
