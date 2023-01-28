d91e976c6bbd51d1287a2f7acb1212d6a3f6f1a9
timbastin <bastin.tim@gmail.com>
2023-01-28 16:58:33 +0100

    Troubleshooting pipeline

--- src/decorators/withAdmin.ts
+++ src/decorators/withAdmin.ts
@@ -1,16 +1,16 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { unstable_getServerSession } from "next-auth";
 import ForbiddenException from "../errors/ForbiddenException";
 import { authOptions } from "../pages/api/auth/[...nextauth]";
 import { ISession } from "../types";
 import { isAdmin } from "../utils/common";
+import { getServerSession } from "../utils/server";
 
 export const withAdmin = async (
   req: NextApiRequest,
   res: NextApiResponse
 ): Promise<ISession | null> => {
   const session: ISession | null =
-    (await unstable_getServerSession(req, res, authOptions)) ?? null;
+    (await getServerSession(req, res, authOptions)) ?? null;
   if (!isAdmin(session)) {
     throw new ForbiddenException();
   }
