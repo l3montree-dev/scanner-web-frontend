f0f41a55f7e9d46c34908d3064ac1b39d3c386db
timbastin <bastin.tim@gmail.com>
2023-02-08 12:02:08 +0100

    Renamed 'Was ist' - to 'jetzt umsetzen!'

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -349,7 +349,7 @@ const Dashboard: FunctionComponent<Props> = (props) => {
                         href={linkMapper[key]}
                         className="text-sm absolute w-full inline-block overflow-hidden truncate text-right whitespace-nowrap top-1 underline right-0 mt-2 px-5"
                       >
-                        Was ist &quot;{mapping[key]}&quot;?
+                        &quot;{mapping[key]}&quot; jetzt umsetzen!
                       </Link>
                     )}
                     <VictoryChart
