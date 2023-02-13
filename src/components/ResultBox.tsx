e0c8aad66b3b956f9ca3585137666be186a71079
timbastin <bastin.tim@gmail.com>
2023-02-13 13:06:26 +0100

    Removed test-server

--- src/components/ResultBox.tsx
+++ src/components/ResultBox.tsx
@@ -63,7 +63,7 @@ const ResultBox: FunctionComponent<Props> = (props) => {
           <Link
             target={"_blank"}
             href={props.link}
-            className="text-sm w-full inline-block  text-right top-1 underline right-0 mt-2 px-5"
+            className="text-sm w-full inline-block  text-right top-1 underline right-0 mt-2"
           >
             {!!props.didPass
               ? `Was ist "${props.title}"?`
