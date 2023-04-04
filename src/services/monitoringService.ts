1d3a4bc336c00d6dc1205052cd4df66ba213159d
timbastin <bastin.tim@gmail.com>
2023-04-04 14:45:38 +0200

    Conditionally building monitoring service

--- src/services/monitoringService.ts
+++ src/services/monitoringService.ts
@@ -2,22 +2,30 @@ import { InfluxDB, Point } from "@influxdata/influxdb-client";
 import { config } from "../config";
 import { getLogger } from "./logger";
 
-const { url, token, org, bucket } = config.influx;
-const writeApi = new InfluxDB({ url, token }).getWriteApi(org, bucket);
-
-const trackSecret = (secret: string) => {
-  const point = new Point("secret").tag("value", secret);
-  writeApi.writePoint(point);
+const logger = getLogger(__filename);
+const buildService = () => {
+  try {
+    const { url, token, org, bucket } = config.influx;
+    const writeApi = new InfluxDB({ url, token }).getWriteApi(org, bucket);
+    return {
+      trackSecret: (secret: string) => {
+        const point = new Point("secret").tag("value", secret);
+        writeApi.writePoint(point);
+      },
+      trackApiCall: (uri: string, secretOrUserId?: string) => {
+        const point = new Point("apiCall")
+          .tag("secretOrUserId", secretOrUserId ?? "")
+          .stringField("uri", uri);
+        writeApi.writePoint(point);
+      },
+    };
+  } catch (e) {
+    logger.warn("Unable to initialize monitoring service", e);
+    return {
+      trackSecret: () => {},
+      trackApiCall: () => {},
+    };
+  }
 };
 
-const trackApiCall = (uri: string, secretOrUserId?: string) => {
-  const point = new Point("apiCall")
-    .tag("secretOrUserId", secretOrUserId ?? "")
-    .stringField("uri", uri);
-
-  writeApi.writePoint(point);
-};
-export const monitoringService = {
-  trackSecret,
-  trackApiCall,
-};
+export const monitoringService = buildService();
