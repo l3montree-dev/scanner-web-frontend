48cedbbbcacfdc9d16232740b36ee557e2c1e8ec
timbastin <bastin.tim@gmail.com>
2023-05-04 12:48:13 +0200

    Added secret protection for info page

--- src/components/bpa/BPAHeader.tsx
+++ src/components/bpa/BPAHeader.tsx
@@ -19,54 +19,56 @@ const BPAHeader: FunctionComponent<Props> = ({ hideLogin }) => {
   const router = useRouter();
   const signOut = useSignOut();
 
+  const query = new URLSearchParams(
+    router.query as Record<string, string>
+  ).toString();
+
   return (
     <header className="border-t-10 z-50 bg-white sticky top-0 border-b-6 border-b-hellgrau-40  border-t-bund">
       <div className="container">
         <div className="flex flex-row justify-between items-center">
           <Logo />
-          <div>
-            <nav className="flex flex-row justify-end">
-              {!Boolean(hideLogin) && (
-                <>
-                  {session.status === "authenticated" ? (
-                    <div className="flex flex-row gap-5">
-                      <SmallLink href="/dashboard">Dashboard</SmallLink>
-                      <span
-                        className="uppercase hover:underline cursor-pointer font-bold text-sm text-dunkelgrau-100"
-                        onClick={() => {
-                          return signOut();
-                        }}
-                      >
-                        Abmelden
-                      </span>
-                    </div>
-                  ) : (
-                    <SmallLink href="/dashboard">Anmelden</SmallLink>
+          {!Boolean(hideLogin) && (
+            <div>
+              <nav className="flex flex-row justify-end">
+                {session.status === "authenticated" ? (
+                  <div className="flex flex-row gap-5">
+                    <SmallLink href="/dashboard">Dashboard</SmallLink>
+                    <span
+                      className="uppercase hover:underline cursor-pointer font-bold text-sm text-dunkelgrau-100"
+                      onClick={() => {
+                        return signOut();
+                      }}
+                    >
+                      Abmelden
+                    </span>
+                  </div>
+                ) : (
+                  <SmallLink href="/dashboard">Anmelden</SmallLink>
+                )}
+              </nav>
+              <nav className="text-gray-600 pt-2 flex flex-row justify-end gap-10">
+                <Link
+                  className={classNames(
+                    activeLink === "/" ? "text-bund" : "text-textblack",
+                    "hover:text-bund"
                   )}
-                </>
-              )}
-            </nav>
-            <nav className="text-gray-600 pt-2 flex flex-row justify-end gap-10">
-              <Link
-                className={classNames(
-                  activeLink === "/" ? "text-bund" : "text-textblack",
-                  "hover:text-bund"
-                )}
-                href="/"
-              >
-                Schnelltest
-              </Link>
-              <Link
-                className={classNames(
-                  activeLink === "/info" ? "text-bund" : "text-textblack",
-                  "hover:text-bund"
-                )}
-                href="/info"
-              >
-                Informationen zur Challenge
-              </Link>
-            </nav>
-          </div>
+                  href={`/?${query}`}
+                >
+                  Schnelltest
+                </Link>
+                <Link
+                  className={classNames(
+                    activeLink === "/info" ? "text-bund" : "text-textblack",
+                    "hover:text-bund"
+                  )}
+                  href={`/info?${query}`}
+                >
+                  Informationen zur Challenge
+                </Link>
+              </nav>
+            </div>
+          )}
         </div>
       </div>
     </header>
