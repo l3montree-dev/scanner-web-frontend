8307bd2488e3dbb5e78f1747bcea0fa00cb33cff
timbastin <bastin.tim@gmail.com>
2023-08-23 09:50:43 +0200

    Added try catch to avoid crash on missing session

--- src/app/(index)/layout.tsx
+++ src/app/(index)/layout.tsx
@@ -1,11 +1,19 @@
-import { FunctionComponent, PropsWithChildren } from "react";
+import { User } from "@prisma/client";
 import BPAHeader from "../../components/bpa/BPAHeader";
-import { GlobalStoreProvider } from "../../zustand/GlobalStoreProvider";
 import { authOptions } from "../../nextAuthOptions";
+import { ISession } from "../../types";
 import { getSessionAndUser } from "../../utils/server";
+import { GlobalStoreProvider } from "../../zustand/GlobalStoreProvider";
 
 const Layout = async ({ children }: any) => {
-  const { session, user } = await getSessionAndUser(authOptions);
+  let session: ISession | undefined;
+  let user: User | undefined;
+  try {
+    const resp = await getSessionAndUser(authOptions);
+    session = resp.session;
+    user = resp.user;
+  } catch {}
+
   return (
     <GlobalStoreProvider session={session} user={user}>
       <div id="index-layout">
