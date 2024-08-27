1940b808814c41bdd5eb611f223b2629ad9247ff
Alexander Selzer <alexander.selzer@aleri.de>
2024-08-27 11:03:59 +0200

    refactor: added NEXT_PUBLIC_DISABLE_DASHBOARD as disableDashboard to feature-flags.ts

--- src/feature-flags.ts
+++ src/feature-flags.ts
@@ -1,3 +1,4 @@
 export const featureFlags = {
   disableRefresh: process.env.NEXT_PUBLIC_DISABLE_REFRESH === "true",
+  disableDashboard: process.env.NEXT_PUBLIC_DISABLE_DASHBOARD === "true",
 };
--- src/scanner/scanner.module.ts
+++ src/scanner/scanner.module.ts
@@ -3,9 +3,10 @@ import { rabbitMQRPCClient } from "../services/rabbitmqClient";
 import { prisma } from "../db/connection";
 import { HashedScanService } from "./hashedScanService";
 import { ScanService } from "./scanService";
+import { featureFlags } from "../feature-flags";
 
 export const scanService = new GlobalRef("scanService", () => {
-  if (process.env.NEXT_PUBLIC_DISABLE_DASHBOARD) {
+  if (featureFlags.disableDashboard) {
     return new HashedScanService(rabbitMQRPCClient, prisma);
   } else return new ScanService(rabbitMQRPCClient, prisma);
 }).value;
