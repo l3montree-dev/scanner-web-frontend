39a8ddb2f581e9ee0d50a8e4d04bc9f30dc429bb
Paul Appelt <paul.appelt@aleri.de>
2024-09-05 12:06:14 +0200

    feat: sitemap.ts uses an imprint URL from .env file or provides default path
    Signed-off-by: Paul Appelt <paul.appelt@aleri.de>

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -6,8 +6,10 @@ const baseUrl = config.canonicalUrl;
 // will be statically optimized at build time
 // therefore we can use the date - which will be the build date
 export default function sitemap(): MetadataRoute.Sitemap {
+  let imprintUrl: string = process.env.NEXT_PUBLIC_IMPRINT_URL || "/impressum";
+
   return [
-    ...["/", "/impressum", "/datenschutz", "/info"].map((entry) => ({
+    ...["/", imprintUrl, "/datenschutz", "/info"].map((entry) => ({
       url: `${baseUrl}${entry}`,
       lastModified: new Date(),
     })),
