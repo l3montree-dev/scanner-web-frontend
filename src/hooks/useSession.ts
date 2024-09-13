f751f3ab01a7b1b8373b7fa808ba20fd9c84bdc9
timbastin <bastin.tim@gmail.com>
2024-09-13 11:42:36 +0200

    fixes wrong type definition

--- src/hooks/useSession.ts
+++ src/hooks/useSession.ts
@@ -2,7 +2,7 @@ import { useSession as baseUseSession } from "next-auth/react";
 import { ISession } from "../types";
 export function useSession() {
   return baseUseSession() as {
-    data: ISession | dashboardEnabledl;
+    data: ISession | null;
     status: string;
   };
 }
